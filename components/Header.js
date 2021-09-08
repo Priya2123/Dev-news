adb300174924477c521acfa95ad8bf9c336c0da3
Priya2123 <aggarwalpriya1121@gmail.com>
2021-09-08 23:40:52 +0530

    WIP

---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import headerStyles from "../styles/Header.module.css";
+
+const Header = () => {
+  const x = 5;
+  return (
+    <div>
+      {/* <h1 className="title">
+        <span>Anything</span> News
+      </h1>
+      <style jsx>
+        {`
+          .title {
+            color: ${x > 3 ? "red" : "blue"};
+          }
+        `}
+      </style> */}
+      <h1 className={headerStyles.title}>
+        <span>Anything</span> News
+      </h1>
+      <p className={headerStyles.description}>
+        Keep up tp date with the latest news
+      </p>
+    </div>
+  );
+};
+
+export default Header;
