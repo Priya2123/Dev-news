18f3e61edaf341fa5cd1d0e362340f261bfaecdc
Priya2123 <aggarwalpriya1121@gmail.com>
2021-09-09 15:29:49 +0530

    WIP

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 // import styles from "../styles/Layout.module.css";
 import Head from "next/head";
-export default function Home() {
+export default function Home({ articles }) {
+  console.log(articles);
   return (
     <div>
       <Head>
@@ -15,3 +16,14 @@ export default function Home() {
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(`http://jsonplaceholder.typicode.com/posts?=limit=6`);
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
